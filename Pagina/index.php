db592c442183e7c161316437dd694a8cf7e6d4bc
Marcelo Schonbrunn <mschonb@gmail.com>
2018-02-13 16:51:54 -0800

    crear navbar

--- Pagina/index.php
+++ Pagina/index.php
@@ -9,7 +9,7 @@
 		<meta name="author" content="">
 		<link rel="icon" href="../../favicon.ico">
 
-		<title>LAB 4: JS</title>
+		<title>Niños y Niñas</title>
 
 		<!-- Bootstrap core CSS -->
 		<link href="css/bootstrap.min.css" rel="stylesheet">
@@ -24,57 +24,47 @@
 		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
-		<script src="https://www.paypalobjects.com/webstatic/ppplusdcc/ppplusdcc.min.js" type="text/javascript"></script>
 	</head>
 
 	<body data-spy="scroll" data-target="#data-scrollspy">
-		<div class="container ">
-			<header class="py-4">
-				<h1>DAW y BD</h1>
-			</header>
-
 			<!-- Static navbar -->
-			<nav class="navbar navbar-expand-lg navbar-light bg-light rounded shadow">
-				<a class="navbar-brand" href="#"><img src="images/logo.png" width="30" height="30" class="d-inline-block " alt=""> </a>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarNav">
-					<ul class="navbar-nav">
-						<li class="nav-item">
-							<a class="nav-link" href="index.html">Home</a>
-						</li>
-						<li class="nav-item dropdown active">
-							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								Labs
-							</a>
-							<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-								<a class="dropdown-item" href="lab1.html">Lab 1</a>
-								<a class="dropdown-item" href="#">Lab 2</a>
-								<a class="dropdown-item" href="lab3.html">Lab 3</a>
-								<a class="dropdown-item active" href="lab4.html">Lab 4</a>
-								<a class="dropdown-item" href="lab5.html">Lab 5/6</a>
-								<a class="dropdown-item" href="lab7.html">Lab 7</a>
-								<a class="dropdown-item" href="lab9.php">Lab 9</a>
-								<div class="dropdown-divider"></div>
-          						<a class="dropdown-item" href="q_1.html">Cuestionario 1</a>
-							</div>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="CV.pdf">CV</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" data-toggle="modal" href="#contact">Contacto</a>
-						</li>
-					</ul>
-				</div>
-			</nav>
-		</div>
+            <nav class="navbar navbar-expand-lg navbar-dark">
+              <a class="navbar-brand" href="#">Niños y Niñas de México</a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
 
-		<div class="container">
-            <div class="jumbotron jumbotron-fluid">asd</div>
-        </div>
-        
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                  <li class="nav-item active">
+                    <a class="nav-link" href="#"> <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Inicio</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      Acerca de Nosotros
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="#">Información General</a><!--Quienes somos, que hacemos, mision, vision, objetivos-->
+                      <a class="dropdown-item" href="#">Areas de atención</a>
+                      <a class="dropdown-item" href="#">Asosiación Civil</a><!--logros y voluntariado, que necesitamos, benefactores-->
+                      <div class="dropdown-divider"></div>
+                    </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Galeria</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Noticias</a>
+                    </li> 
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Calendario</a>
+                    </li> 
+                  </ul>
+              </div>
+            </nav>
 		<footer>
 			<div class="container-fluid">
                 <div class="row">
@@ -101,17 +91,7 @@
                     </div>
                 </div>
             </div>
-            <!--div class="container-fluid py-4">
-				<div class="row">
-					<div class="col-md-6">
-						<span class="text-muted">Hecho con el editor <strong>Brackets</strong> - <a href="http://brackets.io/">http://brackets.io/</a></span>
-					</div>
-					<div class="col-md-3"></div>
-					<div class="col-md-3 text-right align-self-end">
-						Por <strong>Juan Pablo Pino</strong>
-					</div>
-				</div>
-			</div-->
+
 		</footer>
 
 
@@ -119,6 +99,7 @@
 		================================================== -->
 		<!-- Placed at the end of the document so the pages load faster -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="js/comportamiento.js"></script>
 		<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
 		<script src="js/bootstrap.bundle.min.js"></script>
 	</body>
