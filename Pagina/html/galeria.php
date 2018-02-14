baab8ba59b7b48f2ca0302dbfd93930b84c2b055
Marcelo Schonbrunn <mschonb@gmail.com>
2018-02-14 00:47:57 -0800

    Agregar interfaz basica de galeria

---
+++ Pagina/html/galeria.php
@@ -0,0 +1,109 @@
+<HTML>
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        
+        <link rel="stylesheet" href="../css/custom.css">
+    </head>
+    
+    <?php include("partials/_navBar.html")?>
+    
+    <body>
+        <div class="container-fluid" id="galleria">
+            <div class="row">
+                <div class="col-6">
+                    <div class="gallery">
+                      <a target="_blank" href="fjords.jpg">
+                        <img src="../img/Yawui.jpg" alt="Fjords" width="300" height="200">
+                      </a>
+                      <div class="desc">Yawui</div>
+                    </div>
+
+                    <div class="gallery">
+                      <a target="_blank" href="fjords.jpg">
+                        <img src="../img/Yawui.jpg" alt="Fjords" width="300" height="200">
+                      </a>
+                      <div class="desc">Yawui</div>
+                    </div>
+
+                    <div class="gallery">
+                      <a target="_blank" href="fjords.jpg">
+                        <img src="../img/Yawui.jpg" alt="Fjords" width="300" height="200">
+                      </a>
+                      <div class="desc">Yawui</div>
+                    </div>
+
+                    <div class="gallery">
+                      <a target="_blank" href="fjords.jpg">
+                        <img src="../img/Yawui.jpg" alt="Fjords" width="300" height="200">
+                      </a>
+                      <div class="desc">Yawui</div>
+                    </div>
+                    <div class="gallery">
+                      <a target="_blank" href="fjords.jpg">
+                        <img src="../img/Yawui.jpg" alt="Fjords" width="300" height="200">
+                      </a>
+                      <div class="desc">Yawui</div>
+                    </div>
+
+                    <div class="gallery">
+                      <a target="_blank" href="fjords.jpg">
+                        <img src="../img/Yawui.jpg" alt="Fjords" width="300" height="200">
+                      </a>
+                      <div class="desc">Yawui</div>
+                    </div>
+
+                    <div class="gallery">
+                      <a target="_blank" href="fjords.jpg">
+                        <img src="../img/Yawui.jpg" alt="Fjords" width="300" height="200">
+                      </a>
+                      <div class="desc">Yawui</div>
+                    </div>
+
+                    <div class="gallery">
+                      <a target="_blank" href="fjords.jpg">
+                        <img src="../img/Yawui.jpg" alt="Fjords" width="300" height="200">
+                      </a>
+                      <div class="desc">Yawui</div>
+                    </div>
+                    <div class="gallery">
+                      <a target="_blank" href="fjords.jpg">
+                        <img src="../img/Yawui.jpg" alt="Fjords" width="300" height="200">
+                      </a>
+                      <div class="desc">Yawui</div>
+                    </div>
+
+                    <div class="gallery">
+                      <a target="_blank" href="fjords.jpg">
+                        <img src="../img/Yawui.jpg" alt="Fjords" width="300" height="200">
+                      </a>
+                      <div class="desc">Yawui</div>
+                    </div>
+
+                    <div class="gallery">
+                      <a target="_blank" href="fjords.jpg">
+                        <img src="../img/Yawui.jpg" alt="Fjords" width="300" height="200">
+                      </a>
+                      <div class="desc">Yawui</div>
+                    </div>
+                </div>
+                
+                
+                <div class="col-5">
+                    <div class="gallery-large">
+                      <a target="_blank" href="fjords.jpg">
+                        <img src="../img/Yawui.jpg" alt="Fjords" width="300" height="200">
+                      </a>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        
+    </body>
+    
+    
+        <?php
+            include("partials/_footer.html");
+        ?>
+
+
