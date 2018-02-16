191ea4de5e052b8c262e2f72d071c9cb5135e73b
dagopradoa <A01701638@itesm.mx>
2018-02-16 00:17:13 -0600

    change donadores to cards

--- Pagina/donar.php
+++ Pagina/donar.php
@@ -4,35 +4,41 @@ include("html/partials/_header.html");
 ?>
 <!--Esto va en el html pero no lo he metido porque quiero visualizarlo rapido-->
     
-<div id="page-content-wrapper">  
-    <div class="row wrapper">
-        <div class="col-sm-4 col-lg-6">
-            <div class="card">
-                <div class="wrapper">
-                    <h2>Donar via PayPal</h2>
+<div id="page-content-wrapper">
+    <p></p>
+    <div class="container-fluid">
+        <div class="card-deck">
+        <div class="card px-6">
+            <div class="card-block">
+                <div class="container">
+                    <h2 class="card-title">Donar via PayPal</h2>
                     <br>
                     <!--Boton para donar en paypal-->
                     <button type="button" class="btn btn-primary">Dona</button>
                     <br>
                     <br>
-                    
-                    <p>Donación única a través de tu cuenta de paypal</p>
+
+                    <p class="card-text">Donación única a través de tu cuenta de paypal</p>
+                </div>
+            </div>        
+        </div>
+        <div class="card">
+            <div class="card-block ">
+                <div class="container">
+                <h2 class="card-title">Regístrate como donador</h2>
+                <br>
+                <!-- Button trigger modal -->
+                <a class="btn btn-primary" href="php/registrarDonador.php" role="button">
+                  Regístrate
+                </a>
+
+                <p class="card-text py">Nos pondremos en contacto contigo en cuanto termines tu registro</p>
+            
                 </div>
             </div>
         </div>
-        <div class="col-sm-4 col-lg-6">
-            <div class="card">
-                <div class="wrapper">
-                    <h2>Regístrate como donador</h2>
-                    <br>
-                    <!-- Button trigger modal -->
-                    <a class="btn btn-primary" href="php/registrarDonador.php" role="button">
-                      Regístrate
-                    </a>
-
-                    <p>Nos pondremos en contacto contigo en cuanto termines tu registro</p>
-            </div>
         </div>
     </div>
-</div>
+    <p></p>
+    
 <?php include("html/partials/_footer.html");?>
