d7fed82d8f48066c4c0056d6f345dc9dfee05f3f
Joaquin Herrera Ramos <97joaquinhr@gmail.com>
2018-02-15 22:35:44 -0600

    Crear pagina editar y consultar donador

--- Gestor/php/Donadores/consultaD.php
+++ Gestor/php/Donadores/consultaD.php
@@ -2,12 +2,73 @@
 include("../../partials/_header.html");?>
 <div id="page-content-wrapper" class="shadow">
     <!-- Navbar -->
+    <!--Se puede mover todo al html correspondiente-->
     <div class="container-fluid">
         <h1>Consultar donador</h1>
-        
-                
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Campo</th>
+              <th scope="col">Registro</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">Nombre</th>
+              <td>Juan Carlos</td>
+            </tr>
+              <tr>
+              <th scope="row">Apellido Paterno</th>
+              <td>Garzón</td>
+            </tr>
+              <tr>
+              <th scope="row">Apellido Materno</th>
+              <td>Rincón Gallardo</td>
+            </tr>
+              <tr>
+              <th scope="row">RFC</th>
+              <td>GARJ500112MP4</td>
+            </tr>
+              <tr>
+              <th scope="row">Correo Electrónico</th>
+              <td>jgarzon@marhnos.com.mx</td>
+            </tr>
+              <tr>
+              <th scope="row">Dirección</th>
+              <td>Calle Laureles #221, Colonia Jurica, 76060, Querétaro.</td>
+            </tr>
+              <tr>
+              <th scope="row">Teléfono</th>
+              <td>442 235 5641</td>
+            </tr>
+              <tr>
+              <th scope="row">Fecha de nacimiento</th>
+              <td>12/01/1950</td>
+            </tr>
+            <tr>
+              <th scope="row">Modo de pago</th>
+              <td>Tarjeta de Credito</td>
+            </tr>
+            <tr>
+              <th scope="row">Número de cuenta</th>
+              <td>5491 3888 7544 7025</td>
+            </tr>
+            <tr>
+              <th scope="row">Observaciones</th>
+              <td>Fecha de vencimiento: 06/18</td>
+            </tr>
+              <tr>
+              <th scope="row">Uso CFDI</th>
+              <td>G03</td>
+            </tr>        
+          </tbody>
+        </table>
+        <a class="btn btn-primary" href="editarD.php" role="button">
+          Editar
+        </a>
     </div>
 </div>
 <?php
 include("../../partials/_footer.html");
 ?>
+
