33bc300e1f77e41e22b8c4a42d2ace6ca2d50881
Joaquin Herrera Ramos <97joaquinhr@gmail.com>
2018-02-15 18:51:50 -0600

    Crear pagina registroD

--- Pagina/donar.php
+++ Pagina/donar.php
@@ -26,38 +26,11 @@ include("html/partials/_header.html");
                     <h2>Regístrate como donador</h2>
                     <br>
                     <!-- Button trigger modal -->
-                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+                    <a class="btn btn-primary" href="php/registrarDonador.php" role="button">
                       Regístrate
-                    </button>
+                    </a>
 
-                    <!-- Modal -->
-                    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                      <div class="modal-dialog modal-dialog-centered" role="document">
-                        <div class="modal-content">
-                          <div class="modal-header">
-                            <h5>Regístra tus datos</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                              <span aria-hidden="true">&times;</span>
-                            </button>
-                          </div>
-                          <div class="modal-body">
-                              <p>Escribe tus datos</p>
-                              <div class="form-group">
-                                <label for="message-text" class="col-form-label">Message:</label>
-                                <textarea class="form-control" id="message-text"></textarea>
-                              </div>
-                          </div>
-                          <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <br>
-                    <br>
                     <p>Nos pondremos en contacto contigo en cuanto termines tu registro</p>
-                </div>
             </div>
         </div>
     </div>
---
+++ Pagina/php/registrarDonador.php
@@ -0,0 +1,5 @@
+<?php
+include("../html/partials/_headerforfolders.html");
+include("../html/registrarDonador.html");
+include("../html/partials/_footerforfolders.html");
+?>
