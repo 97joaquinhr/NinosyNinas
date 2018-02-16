f447a4d9a2adb812d06694607ec77df1bae1eeb4
Joaquin Herrera Ramos <97joaquinhr@gmail.com>
2018-02-15 22:16:38 -0600

    Create partials in gestor

---
+++ Gestor/php/Donadores/consultaD.php
@@ -0,0 +1,13 @@
+<?php
+include("../../partials/_header.html");?>
+<div id="page-content-wrapper" class="shadow">
+    <!-- Navbar -->
+    <div class="container-fluid">
+        <h1>Consultar donador</h1>
+        
+                
+    </div>
+</div>
+<?php
+include("../../partials/_footer.html");
+?>
