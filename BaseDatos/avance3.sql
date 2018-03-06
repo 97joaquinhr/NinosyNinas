158bd72b78f6f3e883690cac2c545ea5c7b83f6a
dagopradoa <A01701638@itesm.mx>
2018-03-05 18:38:04 -0600

    change sql doc

--- BaseDatos/avance3.sql
+++ BaseDatos/avance3.sql
@@ -152,5 +152,29 @@ Fecha DATETIME
 
 ---------------------------------------------DB FILL--------------------------------------------
 
-SELECT *
-FROM sys.tables
+BULK INSERT equipo07.equipo07.[UsoCFDI]
+   FROM 'e:\wwwroot\equipo07\usoCFDI.csv'
+   WITH 
+      (
+         CODEPAGE = 'ACP',
+         FIELDTERMINATOR = ',',
+         ROWTERMINATOR = '\n'
+      )
+
+BULK INSERT equipo07.equipo07.[MetodoPago]
+   FROM 'e:\wwwroot\equipo07\MetodoPago.csv'
+   WITH 
+      (
+         CODEPAGE = 'ACP',
+         FIELDTERMINATOR = ',',
+         ROWTERMINATOR = '\n'
+      )
+SET DATEFORMAT dmy;
+BULK INSERT equipo07.equipo07.[Donadores]
+   FROM 'e:\wwwroot\equipo07\donadores.csv'
+   WITH 
+      (
+         CODEPAGE = 'ACP',
+         FIELDTERMINATOR = ',',
+         ROWTERMINATOR = '\n'
+      )
