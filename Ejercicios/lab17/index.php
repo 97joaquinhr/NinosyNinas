7c38fd810d96451df2b09ea1d299b8e89d7f3038
Juan Pablo Pino Bravo <a01205525@itesm.mx>
2018-03-06 18:19:15 -0600

    Add footer examples

--- Ejercicios/lab17/index.php
+++ Ejercicios/lab17/index.php
@@ -1,10 +1,12 @@
 <?php
     session_start();
     if(isset($_SESSION["usuario"]) ) {
-        header("location: login.php");    
+        header("location: login.php");
+        // ya esta logeado
     } else {
         include("partials/_header.html");
-        include("partials/_login_form.html");
+        // include("partials/_login_form.html");
+        // mostrar pagina sin iniciar sesion
         include("partials/_footer.html");
     }
 ?>
--- Ejercicios/lab17/login.php
+++ Ejercicios/lab17/login.php
@@ -5,14 +5,14 @@
     if(isset($_SESSION["usuario"]) ) {
         include("partials/_header.html");
         $user = $_SESSION["usuario"];
-        include("partials/_tienda_view.html");
+        // include("partials/_tienda_view.html");
         include("partials/_footer.html");
     } else if (login($_POST["usuario"], $_POST["password"]) ) {
         unset($_SESSION["error"]);
         $_SESSION["usuario"] = $_POST["usuario"];
         $user = $_SESSION["usuario"];
         include("partials/_header.html");
-        include("partials/_tienda_view.html");
+        // include("partials/_tienda_view.html");
         include("partials/_footer.html");
     } else {
         $_SESSION["error"] = "Usuario y/o contraseña incorrectos";
