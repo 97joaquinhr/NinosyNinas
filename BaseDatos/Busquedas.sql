6cb3a0e64a9e445fc65a1218fdc8a3339c50fbac
dagopradoa <A01701638@itesm.mx>
2018-03-07 09:37:09 -0600

    add busquedas.sql

---
+++ BaseDatos/Busquedas.sql
@@ -0,0 +1,14 @@
+SELECT D.Nombre, M.Descripcion
+FROM Donadores D, Donadores_MetodoPago Dm, MetodoPago M
+WHERE D.Email=Dm.Email
+AND Dm.IdMetodo=M.IdMetodo
+AND M.IdMetodo LIKE 'Tarjeta'
+AND D.Ocupacion LIKE 'Maestro'
+
+SELECT F.Nombre
+FROM Funcion F, Rol R, Usuario U, Rol_Funcion Rf, Usuario_Rol Ur
+WHERE F.IdFuncion=Rf.IdFuncion
+AND R.IdRol=Rf.IdRol
+AND U.Username=Ur.Username
+AND R.IdRol=Ur.IdRol
+AND U.Username LIKE 'Marcelo'
