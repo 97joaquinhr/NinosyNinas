b9122dd17bb1f31efc3980d76bd417845b20d830
Joaquin Herrera Ramos <97joaquinhr@gmail.com>
2018-03-07 20:03:07 -0600

    mejorando nav

--- Ejercicios/lab17/consultas.php
+++ Ejercicios/lab17/consultas.php
@@ -1,4 +1,14 @@
+
 <?php
-$p = $_POST["p"];
+session_start();
+require_once("modelo.php");
+$p = $_GET["p"];
+$user = $_SESSION["usuario"];
+$rol = $_SESSION["rol"];
+$privilegios = $_SESSION["privilegios"];
+include("partials/_header.html");
+include("partials/_consultas.html");
+include("partials/_footer.html");
+
 
 ?>
