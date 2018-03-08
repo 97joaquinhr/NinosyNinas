8e9d648c265891e980a25ec37aa6f4059e045ce8
Juan Pablo Pino Bravo <a01205525@itesm.mx>
2018-03-07 19:17:16 -0600

    Add Navbar with privileges

--- Ejercicios/lab17/login.php
+++ Ejercicios/lab17/login.php
@@ -11,8 +11,10 @@
         unset($_SESSION["error"]);
         $_SESSION["usuario"] = $_POST["usuario"];
         $_SESSION["rol"] = getRol($_SESSION["usuario"]);
+        $_SESSION["privilegios"] = getPrivilegios($_SESSION["rol"]);
         $user = $_SESSION["usuario"];
         $rol = $_SESSION["rol"];
+        $privilegios = $_SESSION["privilegios"];
         include("partials/_header.html");
         include("partials/_panel.html");
         include("partials/_footer.html");
