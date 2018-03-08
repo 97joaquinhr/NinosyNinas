6a29affae1978dd942533c998d49756b34a84157
Joaquin Herrera Ramos <97joaquinhr@gmail.com>
2018-03-07 19:19:00 -0600

    phantom changes

--- Ejercicios/lab17/test.php
+++ Ejercicios/lab17/test.php
@@ -3,7 +3,7 @@
 require_once("modelo.php");
 echo getRol('A00000001');
 
-$privilegios = getPrivilegios('A00000001');
+$privilegios = getPrivilegios(getRol('A00000001'));
 print_r($privilegios);
 
 ?>
