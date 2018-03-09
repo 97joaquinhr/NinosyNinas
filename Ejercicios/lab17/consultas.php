0d2dbdf2882e8e2d7d38303d8e826cf7ab94b253
Juan Pablo Pino Bravo <a01205525@itesm.mx>
2018-03-09 15:04:30 -0600

    Add consultas.php bien

--- Ejercicios/lab17/consultas.php
+++ Ejercicios/lab17/consultas.php
@@ -9,10 +9,29 @@
         $user = $_SESSION["usuario"];
         $rol = $_SESSION["rol"];
         $privilegios = $_SESSION["privilegios"];
+        $sub = in_array("RealizarSubConsultas", $privilegios);
 
         include("partials/_header.html");
         include("partials/_nav.html");
+        if ($rol == "Administrador") {
+            switch ($p) {
+                case "TransaccionesRealizadas":
+                    include("partials/_transacciones_monto.html");
+                    break;
+                case "VisualizarPersonalHabilitado":
+                    include("partials/_personal.html");
+                    break;
+
+                default:
+                    # code...
+                    break;
+            }
+        } else {
+            
+        }
         include("partials/_consultas.html");
         include("partials/_footer.html");
+    } else {
+        header("location:index.php");
     }
 ?>
