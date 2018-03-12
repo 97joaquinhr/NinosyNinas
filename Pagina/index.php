6b540371488f2994a8c74a0ed534f3718404d7ea
Linetes <A01207499@itesm.mx>
2018-03-12 02:46:05 -0600

    Mas diseno

--- Pagina/index.php
+++ Pagina/index.php
@@ -1,9 +1,5 @@
 <div class="container">
-    <div class="page-header">
-        <h1>Example Page Header</h1>
-    </div>
-    <p>This is some text.</p>
-    <p>This is another text.</p>
+    <img src="img/logo_notext.png" style="height: 300px;">
 </div>
 <?php
 include("html/partials/_header.html");
