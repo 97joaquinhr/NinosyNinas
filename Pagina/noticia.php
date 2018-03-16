b8109bc86884f6eb270d684f85d5a6fd075201a8
Linetes <A01207499@itesm.mx>
2018-03-16 13:00:09 -0600

    Agregado Noticia

---
+++ Pagina/noticia.php
@@ -0,0 +1,51 @@
+<?php
+include("html/partials/_header.html");
+//include("html/donar.html");
+?>
+<!--Esto va en el html pero no lo he metido porque quiero visualizarlo rapido-->
+    
+<div class="container">
+    <div id="page-content-wrapper">
+        <p></p>
+        <br>
+        <br>
+        <div class="container-fluid">
+            <div class="card-deck">
+                <div class="card shadow round" style="width: 18rem;">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col">
+                                <p><strong>PRECAMPAÑAS: ATAQUES MÁS QUE PROPUESTAS; 35% piensa que AMLO fue más propositivo</strong><br><br><br>
+                                    74% Considera que fueron de poca o nula utilidad para conocer a los abanderados de los partidos; 45% señaló
+                                    que Anaya se centró más en México.
+                                    <br><br>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec pulvinar nisi. Donec eget elit nunc. Cras at mauris viverra, pretium purus at, efficitur neque. Proin rutrum mi ac tincidunt ultricies. Ut sodales non felis vel eleifend. Integer dolor orci, vulputate non dolor fringilla, ultricies lobortis neque. Pellentesque in egestas ex.
+                                    <br><br>
+                                    In eu dui quis libero tristique auctor. Nunc malesuada metus nec sapien commodo, id lacinia elit consectetur. Mauris volutpat feugiat sagittis. Nunc tristique enim non nunc commodo, vitae rutrum orci dapibus. Nam volutpat libero eu nulla blandit tincidunt id a lorem. Nunc aliquam eu felis non feugiat. Morbi quam augue, facilisis dignissim semper id, auctor id nisi. Donec feugiat finibus odio, a mattis metus sollicitudin eget. Proin sed suscipit felis. Vivamus iaculis fringilla enim, id vestibulum dolor sollicitudin a. Phasellus a mi quis lectus euismod placerat rutrum a elit. Praesent congue velit in lobortis mollis. Sed vehicula diam in congue consectetur. Vestibulum accumsan vulputate nisl, a vulputate massa malesuada ac.
+                                    <br><br>
+                                    Phasellus mattis euismod dui a rutrum. Vivamus ligula est, tincidunt ut ultrices ut, ullamcorper nec turpis. Duis rhoncus sem magna, in consequat nisl egestas id. Cras id dui consequat, pellentesque nisl in, consectetur dui. Proin accumsan hendrerit congue. Phasellus est dui, cursus eget sem vel, laoreet bibendum nunc. Sed quis accumsan nibh. Etiam dui ante, sagittis vel urna eu, tristique interdum urna. Aenean placerat accumsan sem, quis tempus mi consequat in.
+                                    <br><br>
+                                    Vivamus facilisis condimentum malesuada. Proin mauris libero, faucibus sit amet leo nec, ornare dictum libero. Nulla nec ex condimentum, facilisis magna nec, tempor dui. Aliquam eu eros a tellus bibendum dignissim non in sem. Phasellus pulvinar vehicula leo, vitae tempor nulla pulvinar ut. Ut ut mauris vel felis dapibus porttitor. Fusce tincidunt ullamcorper ultrices. Morbi semper blandit lacus aliquam imperdiet. Phasellus elementum cursus tempor. Suspendisse euismod varius justo, eget semper massa porta efficitur. Maecenas sit amet suscipit lorem. Sed ultricies libero purus, id hendrerit magna ullamcorper interdum. Suspendisse semper consectetur felis et porttitor.
+                                    <br><br>
+                                    Proin lobortis ante enim, et hendrerit dui pellentesque ac. Nullam id augue quam. Nam lacinia tellus ipsum, eu congue libero eleifend id. Nam auctor erat est, ac dignissim neque sodales sed. Vivamus feugiat ex laoreet lectus faucibus commodo. Nam vel varius sapien. Cras vestibulum mi sit amet ligula sodales auctor. Proin placerat auctor dui et tincidunt. Donec maximus nisi vel enim porta, vel mattis dolor congue. Sed tempus, ligula non efficitur vulputate, est enim lacinia elit, sit amet sagittis nulla nisi eget nisl. Vestibulum rutrum laoreet arcu, eu facilisis turpis. Mauris ultrices blandit libero, at sollicitudin lectus consectetur ac. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+
+
+
+                                </p>
+                            </div>
+                            <div class="col">
+                                <img class="img-thumbnail" src="http://cdn2.excelsior.com.mx/media/styles/imagen_portada_grande/public/pictures/2018/02/15/1861056.jpg" class="img-thumbnail" alt="Elecciones 2018">
+                            </div>
+                        </div>
+                        <a href="noticias.php" class="btn btn-secondary"  role="button" aria-pressed="true">Regresar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <br>
+        <br>
+    </div>
+</div>
+    
+<?php include("html/partials/_footer.html");?>
