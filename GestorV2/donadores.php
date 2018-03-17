9ccfd6a1cfc5712a70f8a006fe4468469ded8def
mschonb <mschonb@gmail.com>
2018-03-17 12:15:00 -0800

    Conectar BD donadores a tabla de Donadores

---
+++ GestorV2/donadores.php
@@ -0,0 +1,9 @@
+<?php
+    require_once("modelo.php");
+    include("partials/_header.html");
+    include("partials/_navbar.html");
+    include("partials/_sidebar.html");
+    include ("partials/_page_content_wrapper_start.html");
+    include("partials/donadores.html");
+
+?>
