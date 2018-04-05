702054b6dab05a134cb06a8a557f127b5dc556a4
Juan Pablo Pino Bravo <a01205525@itesm.mx>
2018-04-04 19:56:30 -0500

    Redesign gallery

--- Pagina/galeria.php
+++ Pagina/galeria.php
@@ -7,7 +7,6 @@ include("html/galeria.html");
 include("../GestorV2/partials/_galeria_modals.html");
 echo   '<script src="https://cdn.jsdelivr.net/gh/atatanasov/gijgo@1.7.3/dist/combined/js/gijgo.min.js" type="text/javascript"></script>
                 <script src="../GestorV2/js/comportamiento.js"></script>
-                <script src="../GestorV2/js/modals_gallery.js"></script>';
+                <script src="js/material-photo-gallery.min.js"></script>';
 echo   '<script src="../GestorV2/js/file_input.js"></script>';
 include("html/partials/_footer.html");
-
