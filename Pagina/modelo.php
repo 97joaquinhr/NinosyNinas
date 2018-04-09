dd3d7a7a39b2d8cb98f1d1fffb9eb4dc58225f9a
mschonb <mschonb@gmail.com>
2018-04-08 17:45:57 -0700

    Trabajar en calendario API

--- Pagina/modelo.php
+++ Pagina/modelo.php
@@ -1,6 +1,6 @@
 <?php
 function connect() {
-    //$mysql = mysqli_connect("127.0.0.1","Linetes","cesarb13","NinosyNinas", 8889);
+   // $mysql = mysqli_connect("localhost","root","","ninos");
     $mysql = mysqli_connect("niyni.tk","dev","1A2b3c4d5e","Niyni");
     $mysql->set_charset("utf8");
     return $mysql;
@@ -8,26 +8,31 @@ function connect() {
 function disconnect($mysql) {
     mysqli_close($mysql);
 }
-function addDonador($email, $rfc,$nombre, $apellidoP, $apellidoM,$fechaN,$direccion,$telefono,$validado, $mp,$observaciones,$cfdi){
+function addDonador($email, $rfc,$nombre, $apellidoP, $apellidoM,$fechaN,$direccion,$telefono, $ocupacion,$validado, $mp,$observaciones,$cfdi){
     $db = connect();
     if ($db != NULL) {
         //Deberiamos usar prepared staments pare evitar SQL Injection
-        $query = 'INSERT INTO `donadores`(`Email`,`RFC`,`Nombre`, `ApellidoPaterno`,`ApellidoMaterno`,`FechadeNacimiento`, `Direccion`,`Telefono`,`Validado`)
-                      VALUES ("'.$email.'", "'.$rfc.'", "'.$nombre.'", "'.$apellidoP.'", "'.$apellidoM.'", "'.$fechaN.'","'.$direccion.'","'.$telefono.'","'.$validado.'")';
+        $query = 'INSERT INTO `donadores`(`Email`,`RFC`,`Nombre`, `ApellidoPaterno`,`ApellidoMaterno`,`FechadeNacimiento`, `Direccion`,`Telefono`,`Ocupacion`,`Validado`)
+                      VALUES ("'.$email.'", "'.$rfc.'", "'.$nombre.'", "'.$apellidoP.'", "'.$apellidoM.'", "'.$fechaN.'","'.$direccion.'","'.$telefono.'","'.$ocupacion.'","'.$validado.'")';
+
         $query2 = 'INSERT INTO donadores_metodopago(`IdMetodo`, `Email`, `Fecha`, `Observaciones`)
                   VALUES("'.$mp.'", "'.$email.'","'.$fechaN.'", "'.$observaciones.'" )';
+
         $query3 = 'INSERT INTO donadores_usocfdi(`Email`,`IdCFDI`,`Fecha`)
                     VALUES("'.$email.'", "'.$cfdi.'", "'.$fechaN.'")';
+
         if (mysqli_query($db, $query)) {
             echo "New record created successfully";
         } else {
             echo "Error: " . $query . "<br>" . mysqli_error($db);
         }
+
         if (mysqli_query($db, $query2)) {
             echo "New record created successfully";
         } else {
             echo "Error: " . $query2 . "<br>" . mysqli_error($db);
         }
+
         if (mysqli_query($db, $query3)) {
             echo "New record created successfully";
         } else {
