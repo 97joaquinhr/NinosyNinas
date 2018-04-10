95f332db2834a5fb59f79b48e2a9d219a436d3f3
Joaquin Herrera Ramos <97joaquinhr@gmail.com>
2018-04-10 08:47:44 -0500

    Trabajo en el calendario, composer para api

--- GestorV2/test.php
+++ GestorV2/test.php
@@ -1,4 +1,24 @@
 <?php
-session_start();
-print_r($_SESSION["what"]);
+set_include_path(get_include_path() . PATH_SEPARATOR . 'C:\xampp\php\library');
+require_once 'Zend/Loader.php';
+Zend_Loader::loadClass('Zend_Gdata_Photos');
+Zend_Loader::loadClass('Zend_Gdata_ClientLogin');
+Zend_Loader::loadClass('Zend_Gdata_AuthSub');
+$serviceName = Zend_Gdata_Photos::AUTH_SERVICE_NAME;
+$user = "ninosyninasdemexico@gmail.com";
+$pass = "educando130";
+
+$client = Zend_Gdata_ClientLogin::getHttpClient($user, $pass, $serviceName);
+
+// update the second argument to be CompanyName-ProductName-Version
+$gp = new Zend_Gdata_Photos($client, "Ninosyninasdemexico-galeria-1.0");
+$query = $gp->newAlbumQuery();
+
+$query->setUser("default");
+$query->setAlbumName("Prueba");
+
+$albumFeed = $gp->getAlbumFeed($query);
+foreach ($albumFeed as $albumEntry) {
+    echo $albumEntry->title->text . "<br />\n";
+}
 ?>
