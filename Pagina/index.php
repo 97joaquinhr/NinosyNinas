900994a35a041300b4b343991ad11d4afd67bcf0
Joaquin Herrera Ramos <97joaquinhr@gmail.com>
2018-04-15 17:58:06 -0500

    work on animated logo

--- Pagina/index.php
+++ Pagina/index.php
@@ -2,5 +2,7 @@
 <?php
 include("html/partials/_header.html");
 include("html/index.html");
+echo '<script src="js/anime.min.js"></script>
+       <script src="js/anime_test.js"></script> ';
 include("html/partials/_footer.html");
 ?>
