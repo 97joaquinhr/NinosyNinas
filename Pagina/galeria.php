be5ee6fbe82f6b6646435348fd8bc7d236c399ba
Joaquin Herrera Ramos <97joaquinhr@gmail.com>
2018-04-15 17:03:02 -0500

    loader for gallery and calendar

--- Pagina/galeria.php
+++ Pagina/galeria.php
@@ -1,15 +1,6 @@
 <?php
-session_start();
-require_once ("../GestorV2/modelo.php");
-
 include("html/partials/_header.html");
 include("html/galeria.html");
-echo   '<script src="js/material-photo-gallery.min.js"></script>
-        <script>
-        var elem = document.querySelector(".m-p-g");
-        document.addEventListener("DOMContentLoaded", function() {
-          var gallery = new MaterialPhotoGallery(elem);
-        });
-        </script>';
+echo   '<script src="js/loader.js"></script>';
 include("html/partials/_footer.html");
 ?>
