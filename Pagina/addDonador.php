4da4aa848a811436055ab9c8139180d39f7586c2
mschonb <mschonb@gmail.com>
2018-04-17 14:19:16 -0500

    Validar Donadores, Registrar desde pagina, Arreglar modificar Donadores

--- Pagina/addDonador.php
+++ Pagina/addDonador.php
@@ -1,5 +1,7 @@
 <?php
-    require_once ("modelo.php");
-    addDonador($_POST["email"], $_POST["rfc"], $_POST["nombre"], $_POST["apellidoP"], $_POST["apellidoM"], $_POST["fechaN"], $_POST["direccion"],$_POST["telefono"], $_POST["ocupacion"],1,$_POST["metodoPago"],$_POST["observaciones"],$_POST["cfdi"]);
-    header("Location: index.php");
-    ?>
+require_once ("../GestorV2/modelo.php");
+$_POST["validado"] = 0;
+addDonador($_POST["email"], $_POST["rfc"], $_POST["nombre"], $_POST["apellidoP"], $_POST["apellidoM"], $_POST["fechaN"], $_POST["direccion"],$_POST["telefono"], "N/A" ,$_POST["validado"],$_POST["metodoPago"],$_POST["observaciones"],$_POST["cfdi"]);
+echo $_POST["validado"];
+
+?>
