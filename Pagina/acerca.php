7a16d24bd30817a3c8d1d9fd92740e2a7d51dd1c
Linetes <A01207499@itesm.mx>
2018-04-24 13:41:39 -0500

    Cambios a modificarporid

--- Pagina/acerca.php
+++ Pagina/acerca.php
@@ -3,4 +3,5 @@ require_once ("../GestorV2/modelo.php");
 include("html/partials/_header.html");
 include("html/acerca.html");
 include("html/partials/_footer.html");
+modificarporID($id, $seccion, $titulo, $descripcion);
 ?>
