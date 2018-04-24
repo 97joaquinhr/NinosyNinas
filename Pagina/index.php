c226fefac8b37714a1bb578b15bba72b08ac75b7
Linetes <A01207499@itesm.mx>
2018-04-24 13:33:43 -0500

    Cambios a modificarporid

--- Pagina/index.php
+++ Pagina/index.php
@@ -1,8 +1,9 @@
-
 <?php
+require_once ("../GestorV2/modelo.php");
 include("html/partials/_header.html");
 include("html/index.html");
 echo '<script src="js/anime.min.js"></script>
        <script src="js/anime_test.js"></script> ';
+echo   '<script src="js/loader.js"></script>';
 include("html/partials/_footer.html");
 ?>
