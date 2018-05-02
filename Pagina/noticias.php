d12160c95609887202c1685e1b2c06e48d25a96f
jp-pino <35382610+jp-pino@users.noreply.github.com>
2018-05-02 10:01:03 -0500

    Update noticias.php

--- Pagina/noticias.php
+++ Pagina/noticias.php
@@ -1,5 +1,5 @@
 <?php
-require_once("../GestorV2/modelo.php");
+require_once("GestorV2/modelo.php");
 include("html/partials/_header.html");
 include("html/noticias.html");
 include("html/partials/_noticias_modals.html");
